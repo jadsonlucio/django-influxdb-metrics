ab5c1fe8ac350e0f37c4de8115fb888914645d7a
jadsonlucio <jadsonaluno@hotmail.com>
2021-06-20 13:39:30 -0300

    fix: converts shared_task to mimick celery.shared_task

--- influxdb_metrics/tasks.py
+++ influxdb_metrics/tasks.py
@@ -4,16 +4,22 @@ from __future__ import absolute_import
 try:
     from celery import shared_task
 except ImportError:
-    def shared_task(func):
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-        return wrapper
+
+    def shared_task(*args, **kwargs):
+        def decorator(func):
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
+
+            return wrapper
+
+        return decorator
+
 
 from .utils import write_points as write_points_normal
 
 
 @shared_task(ignore_result=True)
-def write_points(data, name='influxdb_metrics.tasks.write_points'):
+def write_points(data, name="influxdb_metrics.tasks.write_points"):
     """
     Wrapper around `utils.write_points`.
 
